359d0f607397b72307027181aa912abaa6c18898
6302326797siri-create <6302326797siri@gmail.com>
2026-09-23 12:54:26 +0530

    ✅ Welcome to Java! [java8] — approach1

---
+++ solutions/java8/easy/welcome-to-java/solution-approach1.java
@@ -0,0 +1,19 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/welcome-to-java/problem?isFullScreen=true
+// Problem     Welcome to Java!
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java8
+// Status      Accepted
+// Submitted   2026-09-23, 12:54 p.m.
+// ──────────────────────────────────────────────────
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.");
+System.out.println("Hello, Java.");
+    }
+}
